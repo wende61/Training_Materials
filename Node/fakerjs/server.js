d26eed48a0fc8d966146e8bc90eb357557644f2c
Wendafrash Buzuayehu <Wendafrash.Buzuayehu@safaricom.et>
2024-03-01 15:54:29 +0300

    model added for faker

--- Node/fakerjs/server.js
+++ Node/fakerjs/server.js
@@ -1,49 +1,50 @@
- const { faker } = require('@faker-js/faker');
- const express = require('express');
- const { generateUsers } = require('./model/users');
- const Product = require('./model/product');
- const Order = require('./model/order');
+const { faker } = require('@faker-js/faker');
+const express = require('express');
+const { generateUsers } = require('./model/user');
+const Product = require('./model/product');
+const Order = require('./model/order');
 
 
 
- const app = express();
- const port = 3000;
+const app = express();
+const port = 3000;
 
-//  app.get('/user/:id', (req, res) => {
-//    req.params.id;
-//    req.body;    
-//     const user = {
-//         name: faker.person.fullName(),
-//         gender: faker.person.sex(),
-//         email: faker.internet.email()
-//     };
-//     res.json(user);
-//  });
 
-// app.use((req, res, next) => {
+app.use((req, res, next) => {
 
-//     const authHeader = req.headers.authorizationHeader;
+    const authHeader = req.headers.authorizationHeader;
 
-//     if(!authHeader  || authHeader.indexOf('Basic ') !== 0) {
-//         return res.status(401).json({ message: 'Unauthorized' });
-//     }
+    if (!authHeader || authHeader.indexOf('Basic ') !== 0) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
 
-//     const base64Credintials = authHeader.split(' ')[1];
-//     const credintials = Buffer.from(base64Credintials, 'base64').toString();
-//     const [username, password] = credintials.split(':');
+    const base64Credintials = authHeader.split(' ')[1];
+    const credintials = Buffer.from(base64Credintials, 'base64').toString();
+    const [username, password] = credintials.split(':');
 
-//     if(username !== 'admin' || password !== 'password') {
-//         return res.status(401).json({ message: 'Unauthorized' });
-//     }
+    if (username !== 'admin' || password !== 'password') {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
 
-//     req.user = {
-//         username: 'admin',
-//         email: 'admin@example.com'
-//     };
+    req.user = {
+        username: 'admin',
+        email: 'admin@example.com'
+    };
 
-//     next();
-// });
- 
+    next();
+});
+
+
+app.get('/user/:id', (req, res) => {
+    const user = {
+        id: req.params.id,
+        name: faker.person.fullName(),
+        gender: faker.person.sex(),
+        email: faker.internet.email()
+    };
+    res.json(user);
+});
+
 app.get('/user', (req, res) => {
     const name = req.query.name;
     if (!name) {
@@ -55,11 +56,11 @@ app.get('/user', (req, res) => {
 
 app.get('/product', (req, res) => {
     res.json(Product)
-  })
+})
 
-  app.get('/order', (req, res) => {
+app.get('/order', (req, res) => {
     res.json(Order)
-  })
+})
 
 app.listen(port, () => {
     console.log(`Server Starts at ${port}`)
