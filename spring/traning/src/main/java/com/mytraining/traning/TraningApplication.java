aa6c67d310c1dfaf75e4e4718acc0b1e0da898e9
Wendafrash Buzuayehu <Wendafrash.Buzuayehu@safaricom.et>
2024-03-05 16:30:50 +0300

    update the titorial docs

---
+++ spring/traning/src/main/java/com/mytraining/traning/TraningApplication.java
@@ -0,0 +1,13 @@
+package com.mytraining.traning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraningApplication.class, args);
+	}
+
+}
---
+++ spring/traning/src/test/java/com/mytraining/traning/TraningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytraining.traning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
