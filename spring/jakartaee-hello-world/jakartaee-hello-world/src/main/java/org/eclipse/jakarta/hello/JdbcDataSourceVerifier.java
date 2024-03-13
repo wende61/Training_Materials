58f63d8f45362514a690772acbc5416c8a13d6bd
Wendafrash Buzuayehu <Wendafrash.Buzuayehu@safaricom.et>
2024-03-13 15:50:52 +0300

    xml to database

--- spring/jakartaee-hello-world/jakartaee-hello-world/src/main/java/org/eclipse/jakarta/hello/JdbcDataSourceVerifier.java
+++ spring/jakartaee-hello-world/jakartaee-hello-world/src/main/java/org/eclipse/jakarta/hello/JdbcDataSourceVerifier.java
@@ -1,14 +1,18 @@
 package org.eclipse.jakarta.hello;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
-import javax.activation.DataSource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 import jakarta.annotation.Resource;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.Initialized;
+
 import jakarta.enterprise.event.Observes;
-import jakarta.jms.Connection;
 
 @ApplicationScoped
 public class JdbcDataSourceVerifier {
@@ -16,12 +20,25 @@ public class JdbcDataSourceVerifier {
     @Resource(lookup = "jdbc/postgresql")
     private javax.sql.DataSource dataSource;
 
-    public void init(@Observes @Initialized(ApplicationScoped.class) Object init) throws SQLException {
+    
+    public Connection getConnection() throws NamingException, SQLException{
+        InitialContext ctx = new InitialContext();
+        DataSource dataSource = (DataSource) ctx.lookup("java:comp/env/jdbc/postgresql");
+        return dataSource.getConnection();
+    }
+
+    public void init(@Observes @Initialized(ApplicationScoped.class) Object init) throws SQLException, NamingException {
         System.out.println("Verifying connection to PostgreSQL");
 
-        var metaData = dataSource.getConnection().getMetaData();
+        // Context iniContext = new InitialContext();
+        // Context envContext = (Context) iniContext.lookup("java:comp/env");
+        // DataSource ds = (DataSource) envContext.lookup("jdbc/postgresql");
+        var connection = getConnection();
+        System.out.println("Version: " + connection.getMetaData().getDatabaseMajorVersion() + "." + connection.getMetaData().getDatabaseMinorVersion());
 
-        System.out.println(metaData.getDatabaseProductName());
-        System.out.println("Version: " + metaData.getDatabaseMajorVersion() + "." + metaData.getDatabaseMinorVersion());
+        // var metaData = dataSource.getConnection().getMetaData();
+
+        // System.out.println(metaData.getDatabaseProductName());
+        // System.out.println("Version: " + metaData.getDatabaseMajorVersion() + "." + metaData.getDatabaseMinorVersion());
     }
 }
